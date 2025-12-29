a7c5479e0494ebde60e60dbd4369eabc3be1fd8e
mdlava23-droid <mdlava23@gmail.com>
2025-12-29 07:57:36 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UploadCard from './UploadCard';
+
+function App() {
+  return (
+    <div className="App">
+      <UploadCard />
+    </div>
+  );
+}
+
+export default App;
